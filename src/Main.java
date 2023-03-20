9b7e633b24b8990129c257abe723e7db5d38978f
Si-Thu-WI <C2aung7@gmail.com>
2023-03-20 16:45:48 +0100

    Changed something

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("hello againnn");
+
+        int a = 15;
+        int b = 15;
+        int c = 30+a+b;
     }
 }
