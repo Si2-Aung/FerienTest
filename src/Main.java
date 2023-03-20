4c2f3cd1a784383a1fcc2138b5e2bd41fcdb12e8
Si-Thu-WI <C2aung7@gmail.com>
2023-03-20 16:38:06 +0100

    added changed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("hello again");
+        System.out.println("hello againnn");
     }
 }
